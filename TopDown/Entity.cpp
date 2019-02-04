61822a4ece0850fe65b6c342b5e8550d51820faa
Nikita <niklik45@gmail.com>
2019-02-04 13:32:04 +0300

    GoSide

--- TopDown/Entity.cpp
+++ TopDown/Entity.cpp
@@ -21,7 +21,14 @@ void Entity::CreateSprite()
 
 void Entity::GoSide(int side)
 {
-
+	if (side == 0) Entity::sprite.move(10, 0);
+	if (side == 1) Entity::sprite.move(7.07, 7.07);
+	if (side == 2) Entity::sprite.move(0, 10);
+	if (side == 3) Entity::sprite.move(-7.07, 7.07);
+	if (side == 4) Entity::sprite.move(-10, 0);
+	if (side == 5) Entity::sprite.move(-7.07, -7.07);
+	if (side == 6) Entity::sprite.move(0, -10);
+	if (side == 7) Entity::sprite.move(7.07, -7.07);
 }
 
 
